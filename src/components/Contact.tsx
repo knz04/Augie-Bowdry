b30f1140cfe77cc80f8f25af00602574bf40de36
hawyeet <53439203+knz04@users.noreply.github.com>
2025-08-18 12:45:18 +0700

    fix: change contact component colors

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,9 +4,9 @@ import Form from "./Form";
 
 function Contact() {
   return (
-    <div className="relative flex flex-col md:flex-row md:min-h-[60vh] 2xl:min-h-[50vh] justify-evenly items-center py-12 px-4 bg-neutral-900 overflow-hidden">
+    <div className="relative flex flex-col md:flex-row md:min-h-[60vh] 2xl:min-h-[50vh] justify-evenly items-center py-12 px-4 overflow-hidden">
       <div
-        className="absolute inset-0 z-0 grayscale opacity-15 bg-fixed"
+        className="absolute inset-0 z-0 opacity-100 bg-fixed"
         style={{
           backgroundImage: `url('${contact}')`,
           backgroundSize: "cover",
@@ -15,7 +15,7 @@ function Contact() {
         }}
       ></div>
       <div className="flex flex-col pb-8">
-        <p className="text-2xl md:text-4xl font-semibold text-center transition hover:text-[#2773a6] text-neutral-50 pb-2 z-1">
+        <p className="text-2xl md:text-4xl font-semibold text-center transition hover:text-sky-600 text-neutral-50 pb-2 z-1">
           Let's Get In Touch.
         </p>
         <p className="text-base md:text-xl font-light text-center text-neutral-50 z-1">
@@ -23,7 +23,7 @@ function Contact() {
         </p>
         <Link
           to="/contact"
-          className="text-neutral-50 text-center justify-items-center bg-[#2773a6] hover:bg-[#28679a] hover:cursor-pointer mt-6 py-4 px-4 z-1 shadow-lg transition-all duration-300 ease-in-out transform hover:scale-115"
+          className="text-neutral-50 hover:text-sky-600 text-center justify-items-center bg-sky-600 hover:bg-neutral-50 hover:cursor-pointer mt-6 py-4 px-4 z-1 shadow-lg transition-all duration-300 ease-in-out transform hover:scale-115"
         >
           Contact Us
         </Link>
